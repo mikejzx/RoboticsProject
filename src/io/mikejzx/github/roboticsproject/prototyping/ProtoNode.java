4bad755942c203519976d77f24070c76237ae4a1
Michael <mikeskec2@gmail.com>
2019-03-19 18:48:03 +1100

    update

--- src/io/mikejzx/github/roboticsproject/prototyping/ProtoNode.java
+++
@@ -1,6 +0,0 @@
-package io.mikejzx.github.roboticsproject.prototyping;
-
-public class ProtoNode {
-	public int x = 0;
-	public int y = 0;
-}
--- src/io/mikejzx/github/roboticsproject/prototyping/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * @author Michael
- *
- * This package contains all code used for prototyping the application. 
- * (Specifically: The serialisation and de-serialisation of the data being sent across bluetooth)
- */
-package io.mikejzx.github.roboticsproject.prototyping;
