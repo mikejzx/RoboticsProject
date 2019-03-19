a99f8679da4038e96010a07e3e100d2c1eefdd72
Michael <mikeskec2@gmail.com>
2019-03-19 22:56:06 +1100

    update

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # 2019 Robotics Project (Year 10)
 For Year 10 C.A.T. : Design a robot using an Arduino. This repository contains all source code, including the Main code compiled for the Arduino, the Android App controlling it, and relevant prototyping code.
+
+TODO LIST:
+- [x] - Node position editing application to run on Android.
+- [ ] - Transmit serialised signed 16-bit integer data over Bluetooth to the Arduino, and de-serialise. (W.I.P.)
+- [ ] - Robot is able to steer towards the virtual nodes.
+- [ ] - Ultra-sonic sensor to prevent crashes.
+- [ ] - Show state of robot in app. (Running, completed, etc)
--- RoboticsPrototyping/src/io/mikejzx/github/roboticsproto/ClientDeserialiser.c
+++ RoboticsPrototyping/src/io/mikejzx/github/roboticsproto/ClientDeserialiser.c
@@ -1,5 +1,6 @@
 
 #include <stdio.h>
+#include <stdint.h>
 
 // Very long string put into macros
 #define STR_0 "\nPrototype Application for Michael's Robotics CAT 2019.\
@@ -7,10 +8,14 @@
 the Android Application\n\n"
 
 struct Vector2 {
-    int x, y;
+    int16_t x, y;
 };
 
 int ParseBinary(char[]);
+
+void PrintVector (const struct Vector2* vec) {
+	printf("\n[%d, %d]", vec->x, vec->y);
+}
 
 int main(int argc, char* argv[]) {
     printf(STR_0);
@@ -21,22 +26,26 @@ int main(int argc, char* argv[]) {
 
     printf("Input: %s\n", argv[1]);
 
-    //int serialised = atoi(argv[1]);
-    //printf("\n32-Bit integer: 0x%02X", serialised);
+    // Test buffer emulating that sent by bluetooth.
+    // Is recieved on Arduino
+    // 0->LOWORD, 1->HIWORD
+    unsigned char buffer[] = {
+		0xC8, 0x00, 0x90, 0x01,
+		0xF4, 0x01, 0x58, 0x02, // 0x1F4 And 0x258
+		0xFF, 0x00, 0xCC, 0x00
+    };
 
-    printf("\n0001 0110 parsed = %d", ParseBinary("11111111"));
-
-    /*
-    int length = 17;
-    char* data = "0000110100110011";
-    int current = 0;
-    for (int i = 0; i < length; i++) {
-        current += ParseBinary(*(data + i));
-        if (i % 8 == 0) {
-
-        }
+    // Merge HI & LOW words
+    int size = 12;
+    int nodeCount = size / 4;
+    for (int i = 0; i < nodeCount; i++) {
+    	int i4 = i * 4;
+    	struct Vector2 tmp = {
+    		(int16_t)buffer[i4 + 0] + ((int16_t)buffer[i4 + 1] << 8),
+			(int16_t)buffer[i4 + 2] + ((int16_t)buffer[i4 + 3] << 8)
+    	};
+    	PrintVector(&tmp);
     }
-    */
 
     return 0;
 }
