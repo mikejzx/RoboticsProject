fe3b1a438e79e5c6601265ab593b6d60e5746c93
Michael <mikejzx03@gmail.com>
2019-05-23 20:22:07 +1000

    Lot's of changes. Arduino code is ready for bluetooth testing, and should if all goes well, should be able to deserialise the data.

--- arduino-cat-mainsrc/arduino-cat-mainsrc.ino
+++ arduino-cat-mainsrc/arduino-cat-mainsrc.ino
@@ -23,21 +23,26 @@ const int PORT_MOTOR_SPEEDL = 10, // Left motor speed port
 
 // 2D vectors are used for positions.
 struct Vector2 {
-    // Main members
-    int x, y;
-    
-    // Constructor, just initialise variables.
+    // Main members. Positions are 16-bit integers.
+    // All togethar, a single Vector2 is 32-bits, or 4-bytes in size.
+    uint16_t x, y;
+
+    // Constructors, just initialise variables.
+    Vector2 () : x(0), y(0) {}
     Vector2(int x, int y) : x(x), y(y) {}
 
-    Vector2 Add(const Vector2& a, const Vector2& b) {
-        return Vector2(a.x + b.x, a.y + b.y);    
+    Vector2 add(const Vector2& a, const Vector2& b) {
+        return Vector2(a.x + b.x, a.y + b.y);
     }
 
-    // Basic necessary operator overloads
-    Vector2 operator+ (Vector2& other) { return Vector2::Add(*this, other); }
-    Vector2 operator+=(Vector2& other) {
+    // Basic necessary operator overloads,
+    // can be added to as needed.
+    Vector2 operator+ (const Vector2& other) { 
+        return Vector2::add(*this, other); 
+    }
+    void operator+=(const Vector2& other) {
         this->x += other.x;
-        this->y += other.y;    
+        this->y += other.y;
     }
 };
 
@@ -49,6 +54,7 @@ float steerAmount = 0.0f; // Steering amount from -1.0f to 1.0f
 
 // Line-following vars
 Vector2 currentPos = { 0, 0 };
+Vector2* nodes;
 
 // Miscellanious variables
 uint8_t timer = 0x00;
@@ -88,10 +94,61 @@ void loop() {
         delay(10);
     }
 
+    testBluetooth();
+    
+    // --------------------------
+    // TEST BT BEFORE RUNNING CODE BELOW
+    return;
+    // --------------------------
+
     // Process the actual data sent over bluetooth
-    for (int byteIdx = 0; btSerial.available(); ++byteIdx) {
+    if (btSerial.available()) {
+        // Below here is processing the data sent over
+        // BlueTotth by the Android App on a mobile device.
+        /* Important info: There are currently 2 control-bytes
+                at the beginning of the packet, the first is the 
+                number of nodes that were sent, the second represents
+                the scale of the track in an undetermined unit.
+        */
+
+        // Read serialised bytes
+        unsigned char buffer[sizeof(unsigned char)];
+        size_t packetSize = btSerial.readBytes(buffer, sizeof(buffer));
+        unsigned char nodeCount = buffer[0]; // First control byte: Number of vectors
+        unsigned char trackScale = buffer[1]; // Second control byte: Scale (unused...)
+
+        // Free allocated memory if it exists
+        if (nodes) {
+            delete[] nodes;
+            nodes = new Vector2[nodeCount];
+        }
+
+        // Go through each byte, constructing each Vector2 
+        // from de-serialising the packet as needed
+        // (Start at 2 to skip control bytes)
+        int j = 0, i;
+        for (i = 2; i < nodeCount * 4; i += 4) {
+            // Should be fine allocating this on stack since the
+            // array isn't pointers. So the data is being copied not memory address.
+            Vector2 vec;
+            vec.x = (uint16_t)buffer[i + 0] + ((uint16_t)buffer[i + 1] << 8);
+            vec.y = (uint16_t)buffer[i + 2] + ((uint16_t)buffer[i + 3] << 8);
+            nodes[j] = vec; // Should call the copy constructor
+            ++j;
+        }
+
+        // From here call refresh the track, etc...
+    }
+}
+
+void testBluetooth () {
+    if (Serial.available()) {
+        Serial.println("Bluetooth serial data AVAILABLE");
         // For now just print to the debug monitor.
         Serial.printf("%X ", btSerial.read());
+    }
+    else {
+        Serial.println("No serial data...");
     }
 }
 
